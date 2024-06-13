ececf8af84e7968c08a5906d2e3d70cf7060e5b6
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-06-13 20:43:34 +0500

    добавляет проект с данными

---
+++ SenpaiMarket.DataAccess/Models/BaseTable.cs
@@ -0,0 +1,7 @@
+﻿namespace SenpaiMarket.DataAccess.Models
+{
+    internal abstract class BaseTable
+    {
+        internal Guid Id { get; set; }
+    }
+}
---
+++ SenpaiMarket.DataAccess/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SenpaiMarket.DataAccess.Models
+{
+    /// <summary>
+    /// Продукт
+    /// </summary>
+    internal class Product: BaseTable
+    {
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        [MaxLength(300)]
+        internal string Name { get; set; } = "";
+
+        /// <summary>
+        /// Цена
+        /// </summary>
+        internal decimal Price { get; set; }
+    }
+}
