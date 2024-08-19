e1cab81117b61add7798eb1703c9ad29ec3f68d7
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-08-19 21:32:19 +0500

    настраивает проверку модели dto продукции

---
+++ KartowkaMarkowkaHub.Web/Validators/ProductDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using KartowkaMarkowkaHub.Services.Products;
+
+namespace KartowkaMarkowkaHub.Web.Validators
+{
+    public class ProductDtoValidator : AbstractValidator<ProductDto>
+    {
+        public ProductDtoValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty().WithMessage("Product name is empty");
+            RuleFor(p => p.Price).NotEmpty().WithMessage("Product name is empty")
+                .GreaterThan(0).WithMessage("Product price less than 0");
+        }
+    }
+}
