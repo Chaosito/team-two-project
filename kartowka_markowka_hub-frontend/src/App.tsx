22e760130c3be2d844f4a2744a7a36ce5457b96b
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-11-23 21:20:45 +0500

    создает компонент поиска

--- kartowka_markowka_hub-frontend/src/App.tsx
+++ kartowka_markowka_hub-frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Menu from './Components/Menu';
+import Serarch from './Components/Search';
 
 function App() {
   return (
     <div>
       <Menu/>
+      <Serarch/>
       <h1>App</h1>
     </div>
   );
---
+++ kartowka_markowka_hub-frontend/src/Components/Search.tsx
@@ -0,0 +1,12 @@
+import './../Styles/Search.css';
+
+
+function Serarch() {
+    return <div className='search'>
+        <input/>
+        <button>Найти</button>
+        <span>Усть-Каменогорск</span>
+    </div>
+}
+
+export default Serarch;
