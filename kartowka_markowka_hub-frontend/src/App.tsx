44057c2e6e4d6e5ea015f63abe413b1a22a35cf7
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-11-24 11:21:01 +0500

    создает компонент аутентификации и выделяет цвета в отдельный файл

--- kartowka_markowka_hub-frontend/src/App.tsx
+++ kartowka_markowka_hub-frontend/src/App.tsx
@@ -3,13 +3,14 @@ import './App.css';
 import Menu from './Components/Menu';
 import Serarch from './Components/Search';
 import Footer from './Components/Footer';
+import Login from './Components/Login';
 
 function App() {
   return (
     <div>
       <Menu/>
       <Serarch/>
-      <h1>App</h1>
+      <Login/>
       <Footer/>
     </div>
   );
