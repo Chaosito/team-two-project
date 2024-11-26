fe81d4d8e2e4a2f1fa314f975aeb7e2669e65ec9
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-11-26 19:18:34 +0500

    настраивает маршрутизацию

--- kartowka_markowka_hub-frontend/src/App.tsx
+++ kartowka_markowka_hub-frontend/src/App.tsx
@@ -3,18 +3,25 @@ import './App.css';
 import Menu from './Components/Menu';
 import Serarch from './Components/Search';
 import Footer from './Components/Footer';
-//import Login from './Components/AuthPage/Login';
-//import MainPage from './Components/MainPage/MainPage';
+import Login from './Components/AuthPage/Login';
+import MainPage from './Components/MainPage/MainPage';
 import ProductsPage from './Components/ProductsPage/ProductsPage';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
-function App() {
+function App() {  
   return (
     <div className='app'>
       <div>
         <Menu/>
         <Serarch/>
       </div>
-      <ProductsPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<MainPage/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/products' element={<ProductsPage/>} />
+        </Routes>
+      </BrowserRouter>      
       <Footer/>
     </div>
   );
