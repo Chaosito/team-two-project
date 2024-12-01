c00998ce4a5ba787deff1617b252f89e4bcd9c38
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-12-01 12:31:20 +0500

    создает проект gateway

---
+++ KartowkaMarkowkaHab.Gateway/Program.cs
@@ -0,0 +1,23 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace KartowkaMarkowkaHab.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddOcelot();
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+            var app = builder.Build();
+
+            app.UseOcelot().Wait();
+            //app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
