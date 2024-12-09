ed87466b50ed60eaabbcd61456e614d9b6b4c238
Grigoriy Abashkin <work.grigoriy7mail@gmail.com>
2024-12-09 19:27:18 +0500

    убирает сохраненный логин и пароль и переделывает загрузку продуктов

--- kartowka_markowka_hub-frontend/src/Components/MainPage/NewProducts.tsx
+++ kartowka_markowka_hub-frontend/src/Components/MainPage/NewProducts.tsx
@@ -1,12 +1,42 @@
 import './../../Styles/MainPage/NewProducts.css';
 import ProductCard from '../ProductCard';
 import { Store } from '../../Redux/Store';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { type AppDispatch} from '../../Redux/Store';
+import { useDispatch, useSelector } from 'react-redux';
+import { productsSlice } from '../../Redux/ProductSlice';
 
 function NewProducts() {
+    const baseUrl = process.env.REACT_APP_BASE_URL;
+    let savedToken = localStorage.getItem("myAccessToken") ?? '';
+    const dispatch = useDispatch<AppDispatch>();
+    const { set } = productsSlice.actions;
+
     type RootState = ReturnType<typeof Store.getState>;
     const products = useSelector((state: RootState) => state.products.products);
-    const topProducts = products.slice(-5);
+    const topProducts = products.slice(-5);  
+
+    function loadProducts() {      
+        savedToken = localStorage.getItem("myAccessToken") ?? '';
+        console.log('savedToken: ',savedToken);
+        if(savedToken !== '' && products.length === 0) {
+            fetch(baseUrl + '/api/Product', {
+                method: 'GET',
+                headers: {
+                    "Authorization": "Bearer " + savedToken
+                },        
+            })
+            .then(response => response.json())
+            .then(data => {
+                dispatch(set(data.products));
+            })
+            .catch((error) => console.error(error));
+        }
+    }
+
+    useEffect(() => {
+        loadProducts();
+    }, [savedToken, baseUrl]); 
 
     return <div className="new-products">
         <h3 className='new-products__title'>Свежие продукты</h3>
